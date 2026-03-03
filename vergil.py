8ec2f406ea174f0164adb4db3628ba53366da8ba
Frenchieoui <frenchieouiouioui@gmail.com>
2026-03-02 21:58:39 -0800

    Update vergil.py

--- vergil.py
+++ vergil.py
@@ -2,17 +2,17 @@ import keyboard
 import safeautogui
 import sys
 import time
-from pynput import mouse
 import threading
-import tkinter as tk
 
-enabled = True
+enabled = False
+# press [backspace] key to do auto judgement cut
+# press [y] key to do multiple judgement cuts; YOU MUST set judgement cut keybind to [K]
 
 
 def vergil_judgement_cut():
     safeautogui.keyDown("k")
     safeautogui.keyUp("k")
-    time.sleep(0.65)
+    time.sleep(0.7)
     t = 0.35
     safeautogui.keyDown("k")
     safeautogui.keyUp("k")
@@ -24,26 +24,38 @@ def vergil_judgement_cut():
     safeautogui.keyUp("k")
 
 
+def auto_vergil_judgement_cut():
+    while True:
+        time.sleep(2.25)
+        if enabled:
+            vergil_judgement_cut()
+
+
 def on_key_event(event):
     global enabled
-
-    if event.name == "`" and event.event_type == keyboard.KEY_UP:
-        enabled = not enabled
-        print("Enabled" if enabled else "Disabled")
-
-    if not enabled:
-        return
 
     if event.name == "y" and event.event_type == keyboard.KEY_UP:
         threading.Thread(target=vergil_judgement_cut, daemon=True).start()
 
+    if event.name == "backspace" and event.event_type == keyboard.KEY_UP:
+        enabled = not enabled
+        print("Enable auto" if enabled else "Disabled auto")
+
+    if not enabled:
+        return
+
+
+threading.Thread(target=auto_vergil_judgement_cut, daemon=True).start()
 
 keyboard.hook(on_key_event)
 
 
 print("-------------------------------------------------------------------------------")
-print("Press [`] (back tick) key to enable/disable.")
-print("Press [DELETE] to exit.")
+print("YOU MUST set judgement cut keybind to [K]")
+print("Press [Y] key to do multiple judgement cuts")
+print("Press [backspace] key to enable/disable auto judgement cut.")
+print("Currently Disabled")
+print("Press [DELETE] to exit script.")
 print("-------------------------------------------------------------------------------")
 
 
